4199917a625ce170846b948a2143632f383d62d5
Gaikwad-Aishwarya <aishwaryagaikwad1125@gmail.com>
2024-04-14 15:50:18 +0530

    Files committed

--- App.js
+++ App.js
@@ -1,16 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+
+//React.createElement=>ReactElement->JS object=>HTML element render
+
+//React element
 const heading=React.createElement("h1", {id:"heading"}, "React Project");
 
 console.log(heading);
 
-const jsxHeading=<h1 id="heading">This is JSX Heading</h1>;
+//jsx = transpiled before it reaches the JS=parcel=bable=js to react
+
+//jsx=Babel transpiles it to=>React.createElement=>ReactElement->JS object=>HTML element render
+const jsxHeading=<h1 className="head" tabIndex="1">This is JSX Heading</h1>;
 
 console.log(jsxHeading);
 
+//React Component
+//1.Class based components
+//2.Functional components
+
+/*const HeadingComponent1= () => {
+        <div id="container">
+        return <h1>This is a Functional Component</h1>;
+        </div>
+};
+
+*/
+const HeadingComponent2 = function () {  
+                return(
+                <h1>This is a Functional Component2</h1>)
+        };
+
+        const number = 1000;
+        const elem=<span>React Element</span>
+const HeadingComponents3 = () => { 
+       <div>Hello
+        {/* {heading}
+        {elem}
+
+        <HeadingComponent2 /> //Component Composition
+        <h1 className="heading3">This is a Functional Component3</h1>;
+        <h2>{number}</h2> */}
+        </div>
+};
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+//root.render(jsxHeading);
+root.render(<HeadingComponents3 />);
 
 
 
